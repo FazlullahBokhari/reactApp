c68e983f8e6fd8741d3b20f46e9980804d88bd8d
fazlullahb0 <fazlullahb0@gmail.com>
2020-06-08 17:53:38 +0530

    Updates

---
+++ precache-manifest.43404b4c3a28b32d88f7665837660747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1259135424279d7b4b06a1a62b78051d",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "38f185938cdf9485fe10",
+    "url": "/reactApp/static/css/main.4f1b009d.chunk.css"
+  },
+  {
+    "revision": "c6dbc39c654cbdc39f07",
+    "url": "/reactApp/static/js/2.bec3a786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp/static/js/2.bec3a786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f185938cdf9485fe10",
+    "url": "/reactApp/static/js/main.84477ad0.chunk.js"
+  },
+  {
+    "revision": "30b6a1a43ed5ffe6317a",
+    "url": "/reactApp/static/js/runtime-main.da596ac0.js"
+  },
+  {
+    "revision": "a591a10850070665054beffb4d75e941",
+    "url": "/reactApp/static/media/butter.a591a108.png"
+  }
+]);
